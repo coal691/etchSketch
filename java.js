bc3c880880c96ffc30540a17383c0519ef49df44
coal691 <coleweber17@gmail.com>
2022-09-29 18:51:37 -0500

    the grid has been created

---
+++ java.js
@@ -0,0 +1,7 @@
+
+for(let i = 0; i<255; i++){
+    const grid = document.querySelector(".grid");
+    const grids = document.createElement('div');
+    grids.classList.add('grids');
+    grid.appendChild(grids);
+}
